6ecd736e0259d1ee85e1373c32db8cf73bdd422a
HarryGateaux <antmillar@gmail.com>
2019-11-19 11:21:40 +0000

    first push

---
+++ Assets/GraphController.cs
@@ -0,0 +1,41 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GraphController : MonoBehaviour {
+
+	[SerializeField] Texture2D _texture = null;
+    [SerializeField] Mesh _mesh = null;
+    [SerializeField] Material _material = null;
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+
+//generates random points
+	IEnumerable<Vector3> SetPoints(int count)
+    {
+        while (count > 0)
+        {
+            float x = Random.value;
+            float y = Random.value;
+
+            var color = new Color(1, 1, 1, 1);
+
+            count--;
+            yield return new Vector2(x, y);
+            
+        }
+    }
+
+
+}
+
+
+
