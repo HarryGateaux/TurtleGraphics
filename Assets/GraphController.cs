a1d0339a9a008f14bb25b51a23cac2f18753e7a6
HarryGateaux <antmillar@gmail.com>
2019-11-19 13:03:37 +0000

    added drawline

--- Assets/GraphController.cs
+++ Assets/GraphController.cs
@@ -3,20 +3,24 @@ using System.Collections.Generic;
 using UnityEngine;
 
 public class GraphController : MonoBehaviour {
-
-	[SerializeField] Texture2D _texture = null;
+    [SerializeField] Texture2D _texture = null;
     [SerializeField] Mesh _mesh = null;
     [SerializeField] Material _material = null;
+
+    Mesh _renderMesh;
+    Color32[] _colors;
+
 	// Use this for initialization
-	void Start () {
-		
-	}
+	private void Start () {
+		DrawLine(new Vector3(0, 0, 0), new Vector3(0, 1, 0), new Color(0,0,0));
+    }
 	
 	// Update is called once per frame
 	void Update () {
-		
-	}
 
+
+
+	}
 
 //generates random points
 	IEnumerable<Vector3> SetPoints(int count)
@@ -34,6 +38,20 @@ public class GraphController : MonoBehaviour {
         }
     }
 
+	 void DrawLine(Vector3 start, Vector3 end, Color color, float duration = 0.1f)
+         {
+             GameObject myLine = new GameObject();
+             myLine.transform.position = start;
+             myLine.AddComponent<LineRenderer>();
+             LineRenderer lr = myLine.GetComponent<LineRenderer>();
+             lr.material = new Material(Shader.Find("Particles/Alpha Blended Premultiply"));
+             lr.startColor = color;
+			 lr.endColor = color;
+             lr.startWidth = 0.01f;
+             lr.SetPosition(0, start);
+             lr.SetPosition(1, end);
+             //GameObject.Destroy(myLine, duration);
+         }
 
 }
 
